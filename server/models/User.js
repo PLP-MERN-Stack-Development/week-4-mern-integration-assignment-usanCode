c135d88902f1a4e1555c50eaa267376e8afaac0c
usanCode <sabinerugugula@gmail.com>
2025-07-04 23:20:49 +0200

    Mongoose schema definition for a User model(email, password,role)

---
+++ server/models/User.js
@@ -0,0 +1,11 @@
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["developer" , "admin"], default : "developer" }
+});
+
+module.exports = mongoose.model('User', userSchema);
+// This code defines a Mongoose schema for a User model with fields for email, password, and role.
