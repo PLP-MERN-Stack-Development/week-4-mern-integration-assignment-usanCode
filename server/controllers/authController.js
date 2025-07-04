da0616768c178e0f067e2342fd9bf226f911a960
usanCode <sabinerugugula@gmail.com>
2025-07-05 01:31:56 +0200

    authController.js : Login endpoint logic and added {expireIn : '1h'} in the //signup endpoint logic section .

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,7 +13,26 @@ exports.signup = async (req, res) => {
     const hashed = await bcrypt.hash(password, 10);
     const user = await User.create({ email, password: hashed });
 
-    const token = jwt.sign({ id: user._id , role: user.role }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: user._id , role: user.role }, process.env.JWT_SECRET , {expireIn: '1h'});
     res.json(token);
     
 };
+
+// Login endpoint logic
+
+exports.login = async (req, res) => {   
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: 'User Not Found' });
+    
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(401).json({ message: 'Invalid Password' });
+    
+    const token = jwt.sign({ id: user._id , role: user.role }, process.env.JWT_SECRET , {expireIn : '1h'});
+    res.json(token);
+
+
+
+
+}
