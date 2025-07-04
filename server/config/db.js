6156482b6a0f2aa924eb91d406c5998bf511a8ce
usanCode <sabinerugugula@gmail.com>
2025-07-04 22:36:44 +0200

    db.js file: connection to MongoDB

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,16 @@
 
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 // Connect to MongoDB
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log("MongoDB connected successfully");
+    console.log('MongoDB connected ✅');
   } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
+    console.error('MongoDB connection failed ❌', err);
     process.exit(1); // Exit the process with failure
   }
 };
