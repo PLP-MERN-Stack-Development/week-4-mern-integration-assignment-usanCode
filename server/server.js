7a8ba8681fd76c38b1d31605b06ec2e2a39c57db
usanCode <sabinerugugula@gmail.com>
2025-07-05 16:53:56 +0200

    Configuring the server.js file .

--- server/server.js
+++ server/server.js
@@ -1,3 +1,25 @@
+
+require().config;
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>console.log('Server running on localhost:${PORT}'));
+
+
+
+/*
+
 // server.js - Main server file for the MERN blog application
 
 // Import required modules
@@ -8,9 +30,9 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 // Import routes
-const postRoutes = require('./routes/posts');
-const categoryRoutes = require('./routes/categories');
-const authRoutes = require('./routes/auth');
+//const postRoutes = require('../server/routes/');
+//const categoryRoutes = require('./routes/categories');
+//const authRoutes = require('./routes/auth');
 
 // Load environment variables
 dotenv.config();
@@ -75,4 +97,4 @@ process.on('unhandledRejection', (err) => {
   process.exit(1);
 });
 
-module.exports = app; 
+module.exports = app; */
