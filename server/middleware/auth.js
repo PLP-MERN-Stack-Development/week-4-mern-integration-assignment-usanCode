f7704eeef7971bc1fd2013ff28eb5165e2cba44e
usanCode <sabinerugugula@gmail.com>
2025-07-05 12:10:52 +0200

    auth.js file : Cheks role

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -18,6 +18,17 @@ exports.protect = (req, res, next) => {
      }
   };
 
+  // Checks role
+
+exports.authorize = (roles) => {
+    return (req, res, next) => {
+	    if(!roles.includes(req.user.role)) return res.status(403).json({ message:"Forbidden"});
+	    next();
+
+	};
+
+ };
+
 
   
  
