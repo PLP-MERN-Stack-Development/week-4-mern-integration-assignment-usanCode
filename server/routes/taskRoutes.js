a017a78228afa3aa74f293a0d58f9c41e1f1a43a
usanCode <sabinerugugula@gmail.com>
2025-07-05 16:29:35 +0200

    Creation of >Routes/taskRoutes.js

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,1 +1,14 @@
 
+
+const express = require("express");
+const { createTask, getMyTasks, getAllTasks } = require("../controllers/taskController");
+const { protect, authorize } = require("../middleware/auth");
+const router = express.Router();
+
+
+router.post("/", protect, createTask);
+router.post("/me", protect, getMyTask);
+router.post("/all", protect, authorize(["admin"]), getAllTasks);
+
+module.exports = router;
+
